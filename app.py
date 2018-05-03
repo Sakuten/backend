c6d160c6422e5197816c3baa12474dca1be56cf2
coord.e <me@coord-e.com>
2018-05-04 03:17:00 +0900

    Implement `flask generate` to generate example data into DB for debugging

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def initdb():
 
 @app.cli.command()
 def generate():
-    from api.models import Lottery, Classroom, db
+    from api.models import Lottery, Classroom, User, OAuth2Client, db
+    from werkzeug.security import generate_password_hash, gen_salt
 
     total_index=4
     grades=[5,6]
@@ -38,4 +39,29 @@ def generate():
     classloop(create_classrooms)
     db.session.commit()
     classloop(create_lotteries)
+    db.session.commit()
+
+    def make_debug_user(name):
+        user = User(username=name, passhash=generate_password_hash(name))
+        db.session.add(user)
+        return user
+
+    def make_client(user, scope):
+        options = {'client_name': 'client', 'client_uri': 'http://localhost:5000', 'scope': scope, 'redirect_uri': 'http://localhost:5000/auth', 'grant_type': 'authorization_code\r\npassword', 'response_type': 'code', 'token_endpoint_auth_method': 'client_secret_basic'}
+        client = OAuth2Client(**options)
+        client.user_id = user.id
+        client.client_id = gen_salt(24)
+        if client.token_endpoint_auth_method == 'none':
+            client.client_secret = ''
+        else:
+            client.client_secret = gen_salt(48)
+        db.session.add(client)
+        return (client.client_id, client.client_secret)
+
+    admin = make_debug_user('admin')
+    print(f"admin: {make_client(admin, 'draw')}")
+    for i in range(5):
+        example = make_debug_user(f"example{i}")
+        print(f"example{i}: {make_client(example, 'apply')}")
+
     db.session.commit()
