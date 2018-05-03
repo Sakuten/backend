83d0652ceb3af1304ebd9dd8b23470560a8e0f28
coord.e <me@coord-e.com>
2018-05-03 23:05:40 +0900

    Implement `flask initdb` and use db.sqlite as a file

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ from api.app import create_app
 
 app = create_app({
     'SECRET_KEY': 'secret',
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
 })
+
+
+@app.cli.command()
+def initdb():
+    from api.models import db
+    db.create_all()
