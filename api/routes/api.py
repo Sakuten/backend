368f3cfaf2c26aea7657a580b633a07c40b6fda9
coord.e <me@coord-e.com>
2018-05-04 02:07:36 +0900

    Add full listing API

--- api/routes/api.py
+++ api/routes/api.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, jsonify
 from authlib.flask.oauth2 import current_token
 from api.oauth2 import require_oauth
+from api.models import Lottery, Classroom, db
+from api.schemas import classrooms_schema, lotteries_schema
 
 bp = Blueprint(__name__, 'api')
+
+@bp.route('/classrooms')
+def list_classrooms():
+    classrooms = Classroom.query.all()
+    result = classrooms_schema.dump(classrooms)[0]
+    return jsonify(classrooms=result)
+
+@bp.route('/lotteries')
+def list_lotteries():
+    lotteries = Lottery.query.all()
+    result = lotteries_schema.dump(lotteries)[0]
+    return jsonify(lotteries=result)
 
 @bp.route('/me')
 @require_oauth('profile')
