fa67de7ce17e4c8003a5418ef09aa11c776e24e8
coord.e <me@coord-e.com>
2018-05-09 23:12:06 +0900

    Remove rest of OAuth-related code

--- api/models.py
+++ api/models.py
@@ -1,11 +1,6 @@
 import time
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from authlib.flask.oauth2.sqla import (
-    OAuth2ClientMixin,
-    OAuth2AuthorizationCodeMixin,
-    OAuth2TokenMixin,
-)
 
 db = SQLAlchemy()
 
@@ -27,36 +22,6 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.passhash, password)
 
-
-class OAuth2Client(db.Model, OAuth2ClientMixin):
-    __tablename__ = 'oauth2_client'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(
-        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    user = db.relationship('User')
-
-
-class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
-    __tablename__ = 'oauth2_code'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(
-        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    user = db.relationship('User')
-
-
-class OAuth2Token(db.Model, OAuth2TokenMixin):
-    __tablename__ = 'oauth2_token'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(
-        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    user = db.relationship('User')
-
-    def is_refresh_token_expired(self):
-        expires_at = self.issued_at + self.expires_in * 2
-        return expires_at < time.time()
 
 class Classroom(db.Model):
     id = db.Column(db.Integer, primary_key=True)
