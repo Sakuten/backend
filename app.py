9e5374174777c3f613f8fec7bd6bbb31cbf26fa9
Cj-bc <cj-bc@y-modify.org>
2018-06-13 12:30:28 +0900

    Add: docstring> app.py> add some docstrings
    Added:
            cli command -- initdb
            cli command -- generate
                                    classloop
                                    create_classrooms
                                    make_debug_user
    Left:
            cli command -- generate
                                    create_lotteries

--- app.py
+++ app.py
@@ -10,12 +10,21 @@ app = create_app({
 
 @app.cli.command()
 def initdb():
+    """
+        Initialize Database
+    """
     from api.models import db
     db.create_all()
 
 
+# @app.cli.command define Command-Line commands
+# It's a part of Flask
+
 @app.cli.command()
 def generate():
+    """
+        Geenrate test DB for test
+    """
     from api.models import Lottery, Classroom, User, db
     from werkzeug.security import generate_password_hash
 
@@ -23,15 +32,29 @@ def generate():
     grades = [5, 6]
 
     def classloop(f):
+        """
+            execute function 'f' for all classes
+            Args:
+                f (function): the function to execute.
+        """
         for grade in grades:
             for class_index in range(4):  # 0->A 1->B 2->C 3->D
                 f(grade, class_index)
 
     def create_classrooms(grade, class_index):
+        """
+            create classroom object and add to Database
+            Args:
+                grade (int): set grade. 5 or 6
+                class_index (int): set class_index. 0->A 1->B 2->C 3->D
+        """
         room = Classroom(grade=grade, index=class_index)
         db.session.add(room)
 
     def create_lotteries(grade, class_index):
+        """
+            
+        """
         room = Classroom.query.filter_by(
             grade=grade, index=class_index).first()
         for perf_index in range(total_index):
@@ -44,10 +67,18 @@ def generate():
     db.session.commit()
 
     def make_debug_user(name):
+        """
+            make debug user and add to Database(Should be deleted?)
+            Args:
+                name (int): name of debug user
+            Return:
+                uesr (User): created user instance
+        """
         user = User(username=name, passhash=generate_password_hash(name))
         db.session.add(user)
         db.session.commit()
         return user
+
 
     make_debug_user('admin')
     for i in range(5):
