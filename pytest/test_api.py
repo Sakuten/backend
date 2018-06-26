800425752a8e170620f7286ca1a75432116d472d
Cj-bc <cj-bc@y-modify.org>
2018-06-26 16:48:24 +0900

    Add: pytest> shape of test for '/' and '/api/auth' is added [not completed]

--- pytest/test_api.py
+++ pytest/test_api.py
@@ -1,22 +1,41 @@
 import os
 import tempfile
+import json
 
 import pytest
 
-from api.app import create_app, generate
+from api import app
 from cryptography.fernet import Fernet
 
 @pytest.fixture
-def app():
-    db_fd, database = tempfile.mkstemp()
-    app = create_app({
-        'SECRET_KEY': Fernet.generate_key(),
-        'SQLALCHEMY_TRACK_MODIFICATIONS': False,
-        'SQLALCHEMY_DATABASE_URI': database,
-        'TESTING': True,
-    })
+def client():
+    """make a client for testing
+    """
+    client = app.create_app()
+    db_fd, client.config['DATABASE'] = tempfile.mkstemp()
+    client.config['TESTING'] = True
+    # I have to defined, but I don't know which to set.so, I'll comment out this
+    # client.config['SQLALCHEMY_DATABASE_URI'] = ''
+    test_client = client.test_client()
 
-    yield app
+    yield test_client
 
     os.close(db_fd)
-    os.unlink(app.config['SQLALCHEMY_DATABASE_URI'])
+    os.unlink(client.config['DATABASE'])
+
+
+def post_json(client, url, json_dict):
+    """send POST request to 'url' with 'json_dict' as data
+    """
+    return client.post(url, data=json.dumps(json_dict), content_type='application/json')
+
+def test_toppage(client):
+    resp = client.get('/')
+    assert b'DOC' in resp.data
+
+
+def test_auth(client):
+    """test whether authorization works correctly
+    """
+    resp = post_json(client, '/api/auth/', '{"username":"admin","password":"admin"}')
+    assert b'token' in resp.data
