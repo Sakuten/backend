a1632b7a789ee5ed51c3c70afc6c4b4d35560302
Cj-bc <cj-bc@y-modify.org>
2018-07-05 11:15:13 +0900

    Add: pytest> test 'test_apply_invaild_auth' for /api/lotteries/<id>/apply

--- pytest/test_lottery.py
+++ pytest/test_lottery.py
@@ -112,3 +112,14 @@ def test_apply_invaild_header(client):
 
     assert resp.status_code == 401
 
+def test_apply_invaild_auth(client):
+    """attempt to apply with wrong auth.
+        target_url: /api/lotteries/<id>/apply
+    """
+    idx = '1'
+    token = login(client, test_user['username'], test_user['password'])['token']
+    resp = client.put('/api/lotteries/'+idx+'/apply', headers={'Authorization': 'Bearer test_wrong_token'+ token})
+
+    assert resp.status_code == 401
+
+
