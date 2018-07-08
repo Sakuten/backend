de06e68b5330ac6872f4477dcb4b5f601ca34875
coord.e <me@coord-e.com>
2018-07-08 17:10:34 +0900

    Fix: Lint

--- api/swagger.py
+++ api/swagger.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from flasgger import Swagger, swag_from
 import yaml
 import api
@@ -8,5 +7,7 @@ with open('api/spec/template.yml') as f:
     template = yaml.load(f)
 swag = Swagger(template=template)
 
+
 def spec(path, methods=None):
-    return swag_from(os.path.join(api.__path__[0], 'spec/routes', path), methods=methods)
+    spec_path = os.path.join(api.__path__[0], 'spec/routes', path)
+    return swag_from(spec_path, methods=methods)
