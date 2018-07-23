f0b16aac24151704d5bd1ee195da298e43bfd8bf
tamazasa <ms_020916@yahoo.co.jp>
2018-07-24 00:08:27 +0900

    Add: handle public and secret IDs

--- api/id.py
+++ api/id.py
@@ -1,32 +1,116 @@
 import json
 import random
+from secrets import token_urlsafe
 
 
-maximum_id = 148956
+max_public_id = 34991
 
 
-class IDServer:
-    def __init__(self, json_path):
-        self.json_path = json_path
-        with open(json_path, 'r') as f:
-            json_data = json.load(f)
-            self.unused_id_list = json_data["ids"]
-
-    def pop(self):
-        return self.unused_id_list.pop()
-
-    def update_json_file(self):
-        write_json(self.json_path, self.unused_id_list)
+encoder = "ACDEFGHJKLMNPRTWXY"
 
 
-def write_json(json_path, ids):
-    def id_list2json(ids):
-        return json.dumps({"ids": ids}, indent=4)
+def encode_public_id(num_id):
+    """
+        make 4-letter ID from numeric ID
+        Args:
+            num_id (int): ID consisting of a number and 3 alphabets
+        Return:
+            str_id (str): encoded ID
+    """
+    def one_alpha(num):
+        return encoder[num]
 
-    with open(json_path, 'w') as f:
-        f.write(id_list2json(ids))
+    def one_digit(num):
+        return '9' if num == 5 else str(num + 3)
+    latter_3_num = num_id // (18**3)
+    letters = []
+    for i in range(1, 4):
+        data = num_id % (18**i) // 18**(i-1)
+        letters = [data] + letters
+        sub = data * (18**(i-1))
+        num_id -= sub
+    return one_digit(latter_3_num) + ''.join([one_alpha(n) for n in letters])
+
+
+def decode_public_id(str_id):
+    """
+        make numeric ID from 4-letter ID
+        Args:
+            str_id (str): ID consisting of a number and 3 alphabets
+        Return:
+            num_id (int): numeric ID
+    """
+    def alpha2num(c):
+        return encoder.find(c)
+
+    def num2num(c):
+        return 5 if c == '9' else int(c) - 3
+    alphas = [alpha2num(c) for c in str_id[1:]]
+    alphas.insert(0, num2num(str_id[0]))
+    return sum([alphas[i] * 18**(3-i) for i in range(4)])
+
+
+def find_public_id(ids, secret_id):
+    """
+        search for the public ID that corresponds to the given secret ID
+        Args:
+            ids (list): list of dictionaries that contains IDs
+            secret_id (str): token (target)
+        Return:
+            public_id (str): 4-letter ID
+    """
+    for id_dict in ids:
+        if id_dict["secret_id"] == secret_id:
+            return id_dict["public_id"]
+
+
+def find_secret_id(ids, public_id):
+    """
+        search for the secret ID that corresponds to the given public ID
+        Args:
+            ids (list): list of dictionaries that contains IDs
+            secret_id (str): 4-letter ID (target)
+        Return:
+            public_id (str): token
+    """
+    for id_dict in ids:
+        if id_dict["public_id"] == public_id:
+            return id_dict["secret_id"]
 
 
 def generate_id_json_file(json_path, how_many):
-    ids = random.sample(range(maximum_id), how_many)
-    write_json(json_path, ids)
+    """
+        generate JSON list of dictionaries that have a pair of
+        secret ID and public ID and save it in a JSON file
+        return the generated secret ID for the use in making
+        QR codes
+        Args:
+            json_path (str): path to make JSON file
+            how_many (int): how many pairs of IDs are needed
+        Return:
+            secret_ids (list): all generated secret IDs
+    """
+    public_raw_ids = random.sample(range(max_public_id + 1), how_many)
+    secret_ids = [token_urlsafe(24) for _ in range(how_many)]
+
+    id_dicts = [{"secret_id": secret, "public_id": encode_public_id(public)}
+                for (secret, public) in zip(secret_ids, public_raw_ids)]
+
+    with open(json_path, 'w') as f:
+        f.write(json.dumps(id_dicts, indent=4))
+
+    return secret_ids
+
+
+def load_id_json_file(json_path):
+    """
+        load the JSON file and get the data inside
+        all this function does is to call json.load(f)
+        inside a with statement
+        Args:
+            json_path (str): where the target JSON file is
+        Return:
+            ID list (list): all the data found in the file
+    """
+    with open(json_path, 'r') as f:
+        return json.load(f)
