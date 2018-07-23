e6831f5e1fa2d821db086229da2e83dfbceec1eb
Cj-bc <cj-bc@y-modify.org>
2018-07-23 17:29:34 +0900

    Fix: cards-print> remove non-need var

--- cards-print/config.py
+++ cards-print/config.py
@@ -1,7 +1,6 @@
 # the url to use in QR code
 base_url = 'https://hoge'
 
-vertical = 3    # how much cards listed in vertical line
 target_path = 'test.json'
 
 horizontal = 3  # how much cards listed in horizontal line
