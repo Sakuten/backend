5e6b18841e446514218162f1fd34d4c8ee15f539
Cj-bc <cj-bc@y-modify.org>
2018-07-23 11:11:06 +0900

    Add: cards-print> mkhtml.py
    This outputs HTML file that contains all cards infomations
    based on 'config.py'

---
+++ cards-print/mkhtml.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from jinja2 import Environment, FileSystemLoader
+import json
+
+from mkqr import gen_qr_code
+import card
+import config
+
+
+# 1. Loads list of ids(secret_id, public_id) from json file
+# 2. each 'ids', generate QR code using 'secret_id' and make a 'card' object
+# 3. write HTML file using Jinja2 template
+
+with open(target_path, 'r') as f:
+    id_pairs = json.load(f)
+
+
+qr_pathes = []
+cards = []
+for id_pair in id_pairs:
+    qr = gen_qr_code(qr_url, id_pair['secret_id'])
+    qr_pathes.append(qr)
+
+    newcard = card(qr, id_pair['public_id'])
+    cards.append(newcard)
+
+
+env = Environment(loader=FileSystemLoader('./template'))
+template = env.get_template('cards.html.j2')
+
+html = template.render({'cards':cards, 'horizontal': horizontal, 'vertical': vertical})
