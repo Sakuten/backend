19bbd89748971e87500582d081e6fcae828ab902
Cj-bc <cj-bc@y-modify.org>
2018-07-23 21:11:24 +0900

    Change: cards-print> do> don't make HTML if it already exist

--- cards-print/do.sh
+++ cards-print/do.sh
@@ -5,8 +5,13 @@
 # copyright (c) 2018 Sakutendev
 
 trap 'rm src/qr/*.png src/*.html' 2
-echo "Generating html..."
-echo "Done.\nGenerating PDF..."
 echo "Done.\nAll processes are done properly.exit."
+if ! [ -e src/cards.html ]; then
+  echo "Generating html..."
   pipenv run python mkhtml.py
+  echo "done."
+else
+  echo "html file detected. Skip generating HTML"
+fi
+echo "Generating PDF..."
 wkhtmltopdf --encoding 'utf-8' --lowquality cards.html cards.pdf
