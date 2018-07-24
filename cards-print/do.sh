158f566591dbf3d794136e1766de13ffac0d182d
coord.e <me@coord-e.com>
2018-07-24 10:00:38 +0900

    Fix: Make src/qr directory if not found

--- cards-print/do.sh
+++ cards-print/do.sh
@@ -5,6 +5,11 @@
 # copyright (c) 2018 Sakutendev
 
 trap 'rm src/qr/*.png src/*.html' 2
+
+if ! [ -d src/qr ]; then
+  mkdir -p src/qr
+fi
+
 if ! [ -e src/cards.html ]; then
   echo "Generating html..."
   pipenv run python mkhtml.py
@@ -12,6 +17,7 @@ if ! [ -e src/cards.html ]; then
 else
   echo "html file detected. Skip generating HTML"
 fi
+
 echo "Generating PDF..."
 wkhtmltopdf --encoding 'utf-8' --lowquality src/cards.html cards.pdf
 echo -e "Done.\nAll processes are done properly.exit."
