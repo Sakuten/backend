1286acb87afbbf79aa40dfc0c39498fb2c64abdd
coord.e <me@coord-e.com>
2018-07-25 22:00:51 +0900

    Fix: Fix the range of time

--- api/time_management.py
+++ api/time_management.py
@@ -20,11 +20,11 @@ def get_time_index(time):
     if isinstance(time, datetime.datetime):
         start = current_app.config['START_DATETIME']
         end = current_app.config['END_DATETIME']
-        if not (start < time < end):
-            raise OutOfHourError()
+        if not (start <= time <= end):
+            raise OutOfHoursError()
         # extract datetime.time instance from datetime.datetime
         time = time.time()
 
     for i, (st, en) in enumerate(current_app.config['TIMEPOINTS']):
-        if st < time < en:
+        if st <= time <= en:
             return i
