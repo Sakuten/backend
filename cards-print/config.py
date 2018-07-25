ea642aeb8067608a8c4eeebc8ce4a936140595fb
coord.e <me@coord-e.com>
2018-07-25 12:06:42 +0900

    Change: Pass paths from cmdline args

--- cards-print/config.py
+++ cards-print/config.py
@@ -1,6 +1,4 @@
 # the url to use in QR code
 base_url = 'https://hoge'
 
-target_path = 'test.json'
-
 horizontal = 3  # how much cards listed in horizontal line
--- cards-print/mkhtml.py
+++ cards-print/mkhtml.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import argparse
+
 from jinja2 import Environment, FileSystemLoader
 import json
 
@@ -7,14 +9,17 @@ from mkqr import gen_qr_code
 from cards import card
 from config import *
 
+parser = argparse.ArgumentParser(description='Generate HTML from template')
+parser.add_argument("-i", "--input", type=str, help="Input json file path")
+parser.add_argument("-o", "--output", type=str, help="Output html file path")
+args = parser.parse_args()
 
 # 1. Loads list of ids(secret_id, public_id) from json file
 # 2. each 'ids', generate QR code using 'secret_id' and make a 'card' object
 # 3. write HTML file using Jinja2 template
 
-with open(target_path, 'r') as f:
+with open(args.input, 'r') as f:
     id_pairs = json.load(f)
-
 
 qr_pathes = []
 cards = []
@@ -31,5 +36,5 @@ env = Environment(loader=FileSystemLoader('./template'))
 template = env.get_template('cards.html.j2')
 
 html = template.render({'cards':cards, 'empty_card': empty_card, 'horizontal': horizontal})
-with open('dist/cards.html', 'w') as f:
+with open(args.output, 'w') as f:
     f.write(html)
