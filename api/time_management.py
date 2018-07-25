8205a3fe2bdea46628b07cf2dc8bae9c25f031b9
coord.e <me@coord-e.com>
2018-07-25 20:56:55 +0900

    Add: Add a time management utility module

--- api/time_management.py
+++ api/time_management.py
@@ -1,16 +1,20 @@
-from flask import import current_app
+from flask import current_app
 import datetime
+
 
 class OutOfHourError(Exception):
     pass
 
+
 def get_time_index(time):
     if isinstance(time, datetime.datetime):
         start = current_app.config['START_DATETIME']
-        end   = current_app.config['END_DATETIME']
+        end = current_app.config['END_DATETIME']
         if not (start < time < end):
             raise OutOfHourError()
+        # extract datetime.time instance from datetime.datetime
+        time = time.time()
 
-        for i, (st, en) in enumerate(current_app.config['TIMEPOINTS']):
-            if st < time < en:
-                return i
+    for i, (st, en) in enumerate(current_app.config['TIMEPOINTS']):
+        if st < time < en:
+            return i
