563a353b3f26dba9166888790835e904131bf4ae
Cj-bc <cj-bc@y-modify.org>
2018-07-26 23:06:40 +0900

    Add: (refs #35) pytest> test for token revoking

--- test/test_misc.py
+++ test/test_misc.py
@@ -1,3 +1,9 @@
+from flask import current_app
+import datetime
+from unittest import mock
+from utils import as_user_get, test_user
+
+
 def test_trailing_slash(client):
     """test the behavior of trailing slash
         target_url: /api/classrooms, /api/classrooms/
@@ -8,3 +14,31 @@ def test_trailing_slash(client):
 
     assert resp_with_slash.status_code != 300
     assert resp.get_json() == resp_with_slash.get_json()
+
+
+def test_token_revoke(client):
+    """test tokens are revoked correctly
+        target_url: /api/status (for this test)
+
+        1. test: whether user can use token before the end
+        2. test: wheter user cannot use token after the end
+    """
+    user = test_user
+    with client.application.app_context():
+        end = current_app.config['END_DATETIME']
+    delta = datetime.timedelta(minutes=10)
+    before_end = end - delta
+    after_end = end + delta
+    with mock.patch('api.time_management.get_current_datetime',
+                    return_value=before_end):
+        resp = as_user_get(client, user['username'],
+                           user['g-recaptcha-response'], '/status')
+
+        assert resp.status_code == 200
+
+    with mock.patch('api.time_management.get_current_datetime',
+                    return_value=after_end):
+        resp = as_user_get(client, user['username'],
+                           user['g-recaptcha-response'], '/status')
+
+        assert resp.status_code == 403
