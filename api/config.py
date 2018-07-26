3ff5d322d79491cbb049d9da44652306fb8947bf
coord.e <me@coord-e.com>
2018-07-26 22:44:41 +0900

    Fix: Fix file path treatment

--- api/config.py
+++ api/config.py
@@ -1,5 +1,6 @@
 from cryptography.fernet import Fernet
 import os
+from pathlib import Path
 import api
 
 class BaseConfig(object):
@@ -8,7 +9,8 @@ class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@db/postgres'
     SECRET_KEY = Fernet.generate_key()
-    ID_LIST_FILE = os.path.join(api.__path__, 'cards/ids.json')
+    ROOT_DIR = Path(api.__file__).parent.parent
+    ID_LIST_FILE = ROOT_DIR / Path('cards/ids.json')
     WINNERS_NUM = 90
     RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
 
@@ -24,7 +26,7 @@ class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     ENV = 'development'
-    ID_LIST_FILE = os.path.join(api.__path__, 'cards/test_users.json')
+    ID_LIST_FILE = BaseConfig.ROOT_DIR / 'cards/test_users.json'
     WINNERS_NUM = 3  # just small value
     # Recaptcha test key for automated testing.
     # https://developers.google.com/recaptcha/docs/faq#id-like-to-run-automated-tests-with-recaptcha-v2-what-should-i-do
