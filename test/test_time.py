2791fa76775dd97b485bbc36268cac4c0d4171ff
coord.e <me@coord-e.com>
2018-07-26 09:52:29 +0900

    Add: Add tests for get_draw_time_index

--- test/test_time.py
+++ test/test_time.py
@@ -3,10 +3,57 @@ import pytest
 import datetime
 from api.time_management import (
     mod_time,
+    get_draw_time_index,
     get_time_index,
     OutOfHoursError,
     OutOfAcceptingHoursError
 )
+
+
+def test_draw_time_index_ooh(client):
+    with client.application.app_context():
+        start = client.application.config['START_DATETIME']
+        end = client.application.config['END_DATETIME']
+        res = datetime.timedelta.resolution
+        with pytest.raises(OutOfHoursError):
+            get_draw_time_index(mod_time(start, -res))
+        with pytest.raises(OutOfHoursError):
+            get_draw_time_index(mod_time(end, +res))
+
+
+def test_draw_time_index_ooa(client):
+    with client.application.app_context():
+        timepoints = client.application.config['TIMEPOINTS']
+        ext = client.application.config['DRAWING_TIME_EXTENSION']
+        for i, (_, en) in enumerate(timepoints):
+            res = datetime.timedelta.resolution
+            with pytest.raises(OutOfAcceptingHoursError):
+                get_draw_time_index(mod_time(en, -res))
+            with pytest.raises(OutOfAcceptingHoursError):
+                get_draw_time_index(mod_time(en, +ext+res))
+
+
+def test_draw_time_index_lim(client):
+    with client.application.app_context():
+        timepoints = client.application.config['TIMEPOINTS']
+        ext = client.application.config['DRAWING_TIME_EXTENSION']
+        for i, (_, en) in enumerate(timepoints):
+            res = datetime.timedelta.resolution
+            idx_l = get_draw_time_index(mod_time(en, +res))
+            assert i == idx_l
+            idx_r = get_draw_time_index(mod_time(en, +ext-res))
+            assert i == idx_r
+
+
+def test_draw_time_index_same(client):
+    with client.application.app_context():
+        timepoints = client.application.config['TIMEPOINTS']
+        ext = client.application.config['DRAWING_TIME_EXTENSION']
+        for i, (_, en) in enumerate(timepoints):
+            idx_l = get_draw_time_index(en)
+            assert i == idx_l
+            idx_r = get_draw_time_index(mod_time(en, +ext))
+            assert i == idx_r
 
 
 def test_time_index_ooh(client):
