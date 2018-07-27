5351886ffa707f0294e7e5698feab07c7a9327dd
coord.e <me@coord-e.com>
2018-07-27 22:34:54 +0900

    Fix: Lint

--- scripts/draw.py
+++ scripts/draw.py
@@ -7,10 +7,10 @@
 # but in systemd-timer or cron, etc...
 
 import json
-from urllib.request import Request
+from urllib.request import Request, urlopen
 
-ID_LIST_PATH=""
-API_HOST=""
+ID_LIST_PATH = ""
+API_HOST = ""
 
 id_list = json.load()
 
@@ -20,15 +20,18 @@ admin_cred = next(cred for cred in id_list if cred['authority'] == 'admin')
 # exclude 'authority' key & value from credential
 admin_cred = {k: v for k, v in admin_cred.items() if k != 'authority'}
 
+
 def post_json(url, data=None, token=None):
     headers = {"Content-Type": "application/json"}
     if token:
         headers['Authorization'] = f'Bearer {token}'
     json_data = json.dumps(data).encode("utf-8") if data else None
-    request = Request(f'{API_HOST}/{url}', data=json_data, headers=headers, method='POST')
+    request = Request(f'{API_HOST}/{url}', data=json_data,
+                      headers=headers, method='POST')
     with urlopen(request) as response:
         response_body = response.read().decode("utf-8")
     return json.loads(response_body)
+
 
 # Login as admin
 response = post_json('/auth', admin_cred)
