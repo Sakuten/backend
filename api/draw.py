3533b82c3fc4fbdb4d23193a163f407271fa3157
tamazasa <ms_020916@yahoo.co.jp>
2018-08-06 21:37:32 +0900

    Fix: fix the bug of too few groups winning the lottery

--- api/draw.py
+++ api/draw.py
@@ -36,7 +36,9 @@ def draw_one(lottery):
 
         won_group_members = draw_one_group_members(applications, winners_num)
 
-        rest_winners_num = winners_num - len(won_group_members)
+        # TODO: for unit test until TODO below is done
+        # rest_winners_num = winners_num - len(won_group_members)
+        rest_winners_num = max(winners_num - len(won_group_members), 0)
         won_normal_users = draw_one_normal_users(applications,
                                                  rest_winners_num)
 
@@ -55,39 +57,59 @@ def draw_one_group_members(applications, winners_num):
         decide win or lose for each group
         add applications to the session
     """
+    winner_apps = []
+    loser_apps = []
+    winner_reps = []
+    loser_reps = []
+
+    def set_group_result(rep, is_won):
+        status = "won" if is_won else "lose"
+        to_apps, to_reps = \
+            (winner_apps, winner_reps) if is_won else (loser_apps, loser_reps)
+
+        rep.status = status
+        to_apps.append(rep)
+        to_reps.append(rep)
+
+        for member_id in rep.group_members:
+            member = Application.query.filter_by(user_id=member_id).first()
+            member.status = status
+            to_apps.append(member)
+
+    def unset_group_result(rep, from_apps, from_reps):
+        print(f"unset {rep} from {from_apps}")
+        from_apps.remove(rep)
+        for member_id in rep.group_members:
+            member = Application.query.filter_by(user_id=member_id).first()
+            from_apps.remove(member)
+
     reps = [app for app in applications if app.is_rep]
 
     # How likely is a rep to win
     probability = min(winners_num / len(applications), 1)
 
-    winner_reps = [rep for rep in reps if random.random() < probability]
-
-    winner_apps = set()
-    loser_apps = set()
-
     for rep in reps:
+        set_group_result(rep, random.random() < probability)
 
-        is_won = rep in winner_reps
-        status = "won" if is_won else "lose"
+    n_group_members = sum(len(rep_app.group_members) + 1 for rep_app in reps)
+    n_normal_users = len(applications) - n_group_members
 
-        rep.status = status
-        db.session.add(rep)
-        (winner_apps if is_won else loser_apps).add(rep)
-
-        for member_id in rep.group_members:
-            member = Application.query.filter_by(user_id=member_id).first()
-            member.status = status
-            (winner_apps if is_won else loser_apps).add(member)
-
-    # TODO: needed?
-    # n_group_members = [len(rep_app.group_members) + 1 for rep_app in reps]
+    if len(winner_apps) < winners_num - n_normal_users:
+        # when too few groups accidentally won
+        while loser_reps and len(winner_apps) < winners_num - n_normal_users:
+            new_winner = random.choice(loser_reps)
+            unset_group_result(new_winner, loser_apps, loser_reps)
+            set_group_result(new_winner, True)
 
     if len(winner_apps) > winners_num:
-        # TODO: when too many groups accidentally 'won'
-        pass
-    elif len(winner_apps) < winners_num - sum(winner_reps):
-        # TODO: when too few groups accidentally 'won'
-        pass
+        # when too many groups accidentally won
+        while len(winner_apps) > winners_num:
+            new_loser = random.choice(winner_reps)
+            unset_group_result(new_loser, winner_apps, winner_reps)
+            set_group_result(new_loser, False)
+
+    for user in chain(winner_apps, loser_apps):
+        db.session.add(user)
 
     return winner_apps
 
