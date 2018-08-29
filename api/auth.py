3b369851649e6d4855c3868059389942bcde3088
Cj-bc <cj-bc@y-modify.org>
2018-08-29 19:22:34 +0900

    Change: (refs #78) api/auth> use 'todays_user' before getting 'user' of token owner
    This change, deny all users which was used in other day before.

--- api/auth.py
+++ api/auth.py
@@ -83,7 +83,9 @@ def login_required(*required_authority):
             data = decrypt_token(token)
             if not data:
                 return auth_error(401, 'error="invalid_token"')
-            user = User.query.filter_by(id=data['data']['user_id']).first()
+            user = todays_user(data['data']['user_id'])
+            if user is None:
+                return auth_error(401, 'realm="id_disabled"')
             if required_authority and \
                     (user.authority not in required_authority):
                 return auth_error(403, 'error="insufficient_scope"')
