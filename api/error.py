5d74bc508da31083f21228aedd8f99a2f8fd9274
coord.e <me@coord-e.com>
2018-09-04 03:54:39 +0900

    Change: Take http code from DB, not fron args

--- api/error.py
+++ api/error.py
@@ -2,15 +2,14 @@ from schemas import error_schema
 from models import Error
 
 
-def error_response(code, http_code):
+def error_response(code):
     """
         Construct json response from error code
         Args:
             code(int): Error Code specified in error table json file
-            http_code(int): HTTP Error Code
         Returns:
             resp(Response): flask response with error message
     """
     error = Error.query().filter_by(code=code).first()
     result = error_schema.dump(error)[0]
-    return jsonify(result), http_code
+    return jsonify(result), error.http_code
