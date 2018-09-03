1d32a5051dd00eec283298be25d7385abf2636a4
coord.e <me@coord-e.com>
2018-09-04 03:22:53 +0900

    Change: Include http error code to be handled by error_response

--- api/error.py
+++ api/error.py
@@ -1,14 +1,15 @@
 from schemas import error_schema
 from models import Error
 
-def error_response(code):
+def error_response(code, http_code):
     """
         Construct json response from error code
         Args:
             code(int): Error Code specified in error table json file
+            http_code(int): HTTP Error Code
         Returns:
             resp(Response): flask response with error message
     """
     error = Error.query().filter_by(code=code).first()
     result = error_schema.dump(error)[0]
-    return jsonify(result)
+    return jsonify(result), http_code
