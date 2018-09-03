33fd9dc862822b8ac6f432cb174e3b5fd56dc63e
coord.e <me@coord-e.com>
2018-09-04 03:59:40 +0900

    Add: Add a test of error_response

---
+++ test/test_error.py
@@ -0,0 +1,10 @@
+import pytest
+from api.error import error_response
+
+def test_error_response(client):
+    with client.application.app_context():
+        resp, http_code = error_response(0)
+        assert http_code is not None
+        assert 'message' in resp
+        assert 'code' in resp
+        assert resp['code'] == 0
