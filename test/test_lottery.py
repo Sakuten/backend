35630159ebfe407be5db5bbd8f96402ace0407f5
coord.e <me@coord-e.com>
2018-09-07 01:36:56 +0900

    Delete: Delete a test which depends on initial data

--- test/test_lottery.py
+++ test/test_lottery.py
@@ -98,7 +98,6 @@ def test_get_all_available_lotteries(client):
         resp = client.get('/lotteries/available')
 
     assert current_lotteries == resp.get_json()
-    assert len(resp.get_json()) == 8
 
 
 def test_get_specific_lottery(client):
