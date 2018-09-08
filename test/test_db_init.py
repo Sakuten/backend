49c684c91628e3e5a92ffc64087c313d25f9b8c7
coord.e <me@coord-e.com>
2018-09-08 17:14:45 +0900

    Add: Add a test of DB_FORCE_INIT

---
+++ test/test_db_init.py
@@ -0,0 +1,14 @@
+from api.models import db, Classroom
+from api.app import create_app, init_and_generate
+
+def test_db_force_init(client):
+    with client.application.app_context():
+        dummy = Classroom(grade=0, index=0)
+        db.session.add(dummy)
+        db.session.commit()
+        dummy_id = dummy.id
+        assert Classroom.query.get(dummy_id) is not None
+
+        client.application.config['DB_FORCE_INIT'] = True
+        init_and_generate()
+        assert Classroom.query.get(dummy_id) is None
