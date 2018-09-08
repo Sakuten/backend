66623b34687b37cb81d5d97da9a8c45c3f5018c6
coord.e <me@coord-e.com>
2018-09-08 17:18:43 +0900

    Change: Use parametrize to test both force inited or not

--- test/test_db_init.py
+++ test/test_db_init.py
@@ -1,7 +1,10 @@
+import pytest
+
 from api.models import db, Classroom
 from api.app import create_app, init_and_generate
 
-def test_db_not_force_init(client):
+@pytest.mark.parametrize("force_init", [True, False])
+def test_db_init(client, force_init):
     with client.application.app_context():
         dummy = Classroom(grade=0, index=0)
         db.session.add(dummy)
@@ -9,18 +12,9 @@ def test_db_not_force_init(client):
         dummy_id = dummy.id
         assert Classroom.query.get(dummy_id) is not None
 
-        client.application.config['DB_FORCE_INIT'] = False
+        client.application.config['DB_FORCE_INIT'] = force_init
         init_and_generate()
-        assert Classroom.query.get(dummy_id) is not None
-
-def test_db_force_init(client):
-    with client.application.app_context():
-        dummy = Classroom(grade=0, index=0)
-        db.session.add(dummy)
-        db.session.commit()
-        dummy_id = dummy.id
-        assert Classroom.query.get(dummy_id) is not None
-
-        client.application.config['DB_FORCE_INIT'] = True
-        init_and_generate()
-        assert Classroom.query.get(dummy_id) is None
+        if force_init:
+            assert Classroom.query.get(dummy_id) is None
+        else:
+            assert Classroom.query.get(dummy_id) is not None
