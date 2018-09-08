8e2875067698634cef037514c66c81349e9ee8c6
coord.e <me@coord-e.com>
2018-09-08 17:45:24 +0900

    Add: Add a test to check the behavior of 'always'

--- test/test_db_generate.py
+++ test/test_db_generate.py
@@ -39,8 +39,25 @@ def test_db_generate_first_time(client):
         assert len(Lottery.query.all()) != 0
 
         changed = False
-        init_and_generate()  # initial data generated, so does nothing
+        init_and_generate()  # initial data is already generated, so does nothing
         event.remove(db.session, 'before_commit', detect_change)
 
         assert not changed
 
+def test_db_generate_always(client):
+    with client.application.app_context():
+        client.application.config['DB_GEN_POLICY'] = 'always'
+
+        changed = False
+
+        @event.listens_for(db.session, 'before_commit')
+        def detect_change(*args,**kw):
+            nonlocal changed
+            changed = True
+
+        # initial data is already generated,
+        # However re-generates the data
+        init_and_generate()
+        event.remove(db.session, 'before_commit', detect_change)
+
+        assert changed
