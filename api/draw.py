79b549b1b61f3e646574fb4a8c957f945f4e46af
tamazasa <ms_020916@yahoo.co.jp>
2018-10-29 14:40:07 +0900

    Change: guarantee len(winners) == WINNERS_NUM

--- api/draw.py
+++ api/draw.py
@@ -101,17 +101,19 @@ def draw_one_group_members(applications, winners_num):
                           for rep in reps)
     n_normal_users = len(applications) - n_group_members
 
-    # when too few groups accidentally won
-    while loser_reps and len(winner_apps) < winners_num - n_normal_users:
-        new_winner = random.choice(loser_reps)
-        unset_group_result(new_winner, loser_apps, loser_reps)
-        set_group_result(new_winner, True)
+    while (loser_reps and len(winner_apps) < winners_num - n_normal_users or
+           len(winner_apps) > winners_num):
+        # when too few groups accidentally won
+        while loser_reps and len(winner_apps) < winners_num - n_normal_users:
+            new_winner = random.choice(loser_reps)
+            unset_group_result(new_winner, loser_apps, loser_reps)
+            set_group_result(new_winner, True)
 
-    # when too many groups accidentally won
-    while len(winner_apps) > winners_num:
-        new_loser = random.choice(winner_reps)
-        unset_group_result(new_loser, winner_apps, winner_reps)
-        set_group_result(new_loser, False)
+        # when too many groups accidentally won
+        while len(winner_apps) > winners_num:
+            new_loser = random.choice(winner_reps)
+            unset_group_result(new_loser, winner_apps, winner_reps)
+            set_group_result(new_loser, False)
 
     for user in chain(winner_apps, loser_apps):
         db.session.add(user)
